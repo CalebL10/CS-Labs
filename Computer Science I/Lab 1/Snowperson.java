62516ae1a361b0ffda9ff11f0eb39dcfaa58cf53
Caleb <90160115+CalebL10@users.noreply.github.com>
2024-09-19 07:38:02 -0400

    Add files via upload

---
+++ Computer Science I/Lab 1/Snowperson.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Snowperson here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Snowperson
+{
+    public static void main(String[] args)
+    {
+        System.out.println("          ****");
+        System.out.println("        *::::::*");
+        System.out.println("      *::::::::::*");
+        System.out.println("    *::::o::::0::::*");
+        System.out.println("   *::::::::T::::::::*");
+        System.out.println("    *:::::::::::::::*");
+        System.out.println("      *::::___::::*");
+        System.out.println("        *:::::::*"); 
+        System.out.println("          *****");
+        System.out.println("  *:::::::::H:::::::::*");
+        System.out.println(" *::::::::::H::::::::::*");
+        System.out.println("*\\::::::::::H::::::::::/*");
+        System.out.println(" *::::::::::H::::::::::*");
+        System.out.println("  *:::::::::H:::::::::*");
+        System.out.println("   *::::::::H::::::::*");
+        System.out.println("    *:::::::H:::::::*");
+        System.out.println("     *::::::H::::::*");
+        System.out.println("      *************");
+    }
+}
